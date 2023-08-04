c254e1af83151cec3b07131865f65f99bd40053c
Steven Imhof <steven.imhof@raiffeisen.ch>
2023-08-04 08:45:13 +0200

    setup

--- apps/normal-app/src/app/app.component.ts
+++ apps/normal-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
+  imports: [NxWelcomeComponent, NgbAlertModule],
   selector: 'nx-mf-treeshaking-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
